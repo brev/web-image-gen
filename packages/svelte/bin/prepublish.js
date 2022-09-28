870e88fe9b675e6d1eba0aa753dfdaddb5f433de
Brev Patterson <me@brev.name>
2022-09-27 22:15:58 -0700

    Simplify root publish warning.

--- packages/svelte/bin/prepublish.js
+++
@@ -1,7 +0,0 @@
-const PUBLISH_CONFIRM = !!(process.env.PUBLISH_CONFIRM)
-
-if (!PUBLISH_CONFIRM) {
-  console.error("!!! Confirm flag and dir: `PUBLISH_CONFIRM=1 pnpm publish package/` !!!\n")
-  process.exit(1)
-}
-
