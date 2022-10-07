48968bd11d7b7627ba4b1e47650bc83d4059cb0e
Brev Patterson <me@brev.name>
2022-10-06 20:02:14 -0700

    don't re-rexport const and stub from common, user can install that pkg on their own if they want those. Decouples const/stub from all other dependencies (sharp, etC).

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,13 +1,6 @@
 import { checkConfig, defaultConfig, getConfig } from './config.js'
 import clean from './clean.js'
-import { creditStub, imageSetStub } from 'web-image-gen-common/stub'
 import generate from './generate.js'
-import {
-  imageContentTypes,
-  imageInputFormats,
-  imageOutputFormats,
-  manifestOutputFormats,
-} from 'web-image-gen-common/const'
 import optimize from './optimize.js'
 import remove from './remove.js'
 
@@ -21,17 +14,11 @@ export type {
 } from 'web-image-gen-common'
 
 export {
-  creditStub,
   checkConfig,
   clean,
   defaultConfig,
   generate,
   getConfig,
-  imageContentTypes,
-  imageInputFormats,
-  imageOutputFormats,
-  imageSetStub,
-  manifestOutputFormats,
   optimize,
   remove,
 }
